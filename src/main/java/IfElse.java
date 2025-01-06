0c8708ac5d31a7570916cbf4c40666190898dfe6
Dani <iddolphin2007@mail.com>
2025-01-06 08:56:26 +0300

    Commit3

--- src/main/java/IfElse.java
+++ src/main/java/IfElse.java
@@ -1,5 +1,18 @@
+import java.time.Instant;
+import java.util.Iterator;
+import java.util.PrimitiveIterator;
+import java.util.function.IntConsumer;
+import java.util.stream.IntStream;
+
 public class IfElse {
     public static void main(String[] args) {
+        IntStream iterator = IntStream.range(1,100);
+        iterator.forEach(System.out::println);
+
+
+
+
+
         int value=10;
         if (value<0){
             System.out.println(value);
